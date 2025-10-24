5fcccf25f35f5c3e67697f0b6e0ba05f9732b382
brennogf <brennogivigier@gmail.com>
2025-10-24 01:47:31 -0300

    Add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,22 @@
+import cors from 'cors';
 import express from 'express';
-import path from 'path'
-import routes from './routes'
-const PORT = process.env.PORT || 4000
+import path from 'path';
+import routes from './routes';
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 
-app.use(express.static(path.join(__dirname, "../../frontend/build")))
+app.use(express.static(path.join(__dirname, '../../frontend/build')));
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta: ${PORT}`)
-})
+  console.log(`Servidor rodando na porta: ${PORT}`);
+});
